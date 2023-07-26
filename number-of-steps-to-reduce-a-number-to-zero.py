4487d29bb9482c3e8e45000f7e122df04ec4e27f
dan m <dm@danmarius.dev>
2023-07-26 20:26:08 +0200

    modified:   number-of-steps-to-reduce-a-number-to-zero.py

--- number-of-steps-to-reduce-a-number-to-zero.py
+++ number-of-steps-to-reduce-a-number-to-zero.py
@@ -10,42 +10,36 @@ class Solution:
                 steps += 1
         return steps
 
-    """
-    other solutions
+"""
+Other solutions:
+########### 1 ##############
+#    Time:   O(log(n))     #
+#    Memory: O(log(n))     #
+############################
+def numberOfSteps(self, num: int) -> int:
+    if num == 0:
+        return 0
+    return 1 + self.numberOfSteps(num - 1 if num & 1 else num >> 1)
 
-    # 1.     
-    
-    Time:   O(log(n))
-    Memory: O(log(n))
+########### 2 ##############
+#    Time:   O(log(n))     #
+#    Memory: O(1)          #
+############################
+def numberOfSteps(self, num: int) -> int:
+    steps = 0
+    while num != 0:
+        steps += 1
+        if num & 1:
+            num -= 1
+        else:
+            num >>= 1
+    return steps
 
-        def numberOfSteps(self, num: int) -> int:
-        if num == 0:
-            return 0
-        return 1 + self.numberOfSteps(num - 1 if num & 1 else num >> 1)
-
-    # 2.
-    
-    Time:   O(log(n))
-    Memory: O(1)
-
-        def numberOfSteps(self, num: int) -> int:
-        steps = 0
-
-        while num != 0:
-            steps += 1
-            if num & 1:
-                num -= 1
-            else:
-                num >>= 1
-
-        return steps
-    
-    # 3.
-
-    Time:   O(1)
-    Memory: O(1)
-
-        def numberOfSteps(self, num: int) -> int:
+########### 3 ##############
+#    Time:   O(1)          #
+#    Memory: O(1)          #
+############################
+    def numberOfSteps(self, num: int) -> int:
         if num == 0:
             return 0
         return num.bit_length() - 1 + num.bit_count()
