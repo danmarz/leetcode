dd8b7c6c4d21e0fb018aa49bc5e62ddbc2145a9d
dan m <dm@danmarius.dev>
2023-07-27 23:03:20 +0200

    new file:   middle-of-the-linked-list.py

---
+++ middle-of-the-linked-list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        arr = [head]
+        while arr[-1].next:
+            arr.append(arr[-1].next)
+        return arr[len(arr) // 2]
+
+
+# Alternative solution:
+# class Solution:
+#     def middleNode(self, head):
+#         slow = fast = head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         return slow
