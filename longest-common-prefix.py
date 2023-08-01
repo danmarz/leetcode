59ea3976a706da8fd6d8a0e2962d78dbe9b0478a
dan m <dm@danmarius.dev>
2023-08-01 13:41:34 +0200

    modified:   longest-common-prefix.py

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -12,3 +12,20 @@ class Solution:
             common += first[i]
 
         return common
+
+
+# Alternative solution (try/except block with IndexError)
+"""
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(strs[0])):
+            sym = strs[0][i]
+
+            for s in strs:
+                try:
+                    if s[i] != sym:
+                        return strs[0][:i]
+                except IndexError:
+                    return strs[0][:i]
+        return strs[0]
+"""
