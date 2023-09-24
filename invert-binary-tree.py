3bab57e50b26f1e5c12b6432addeff69c60cd923
dan m. <dm@danmarius.dev>
2023-09-24 14:28:08 +0200

    new file:   invert-binary-tree.py

---
+++ invert-binary-tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
+
+        # More verbose
+        """ 
+        if not root:
+                return None
+            
+        # Swap the left and right subtrees
+        root.left, root.right = root.right, root.left
+            
+        # Recursively invert the left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+            
+        return root
+        """
