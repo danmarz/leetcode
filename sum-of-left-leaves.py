a71475b43b1b1237210046abf5241db94a3dca05
dan m. <dm@danmarius.dev>
2023-10-29 16:19:34 +0100

    modified:   sum-of-left-leaves.py

--- sum-of-left-leaves.py
+++ sum-of-left-leaves.py
@@ -6,9 +6,41 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        # initialize an empty queue
+        queue = []
+        # add the root node to the queue with a flag ('root' - root, 'l' - left, 'r' - right)
+        queue.append((root, "root"))
+        # initialize the answer
+        ans = 0
+        # if the root is empty, return 0
         if not root:
             return 0
+        # while the queue is not empty
+        while len(queue) > 0:
+            # get the size of the queue
+            size = len(queue)
+            # pop the node from the queue
+            node, flag = queue.pop(0)
+            # check if the node has a left child
+            if node.left:
+                # append the left child to the queue with a flag
+                queue.append((node.left, "l"))
+            # check if the node has a right child
+            if node.right:
+                # append the right child to the queue with a flag
+                queue.append((node.right, "r"))
+            # check if the node is a left leaf
+            if not node.left and not node.right and flag == "l":
+                # add its value to the answer
+                ans += node.val
+        # return the answer
+        return ans
 
+        """ 
+        # Recursive approach
+        if not root:
+            return 0
+        
         left_sum = 0
 
         # Check if the left child of the current node is a leaf (no left or right child).
@@ -20,3 +52,4 @@ class Solution:
         left_sum += self.sumOfLeftLeaves(root.right)
 
         return left_sum
+        """
