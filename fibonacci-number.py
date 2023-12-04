2e88a354360467ee0972bc879a65a1cbcae9a5b6
dan m. <dm@danmarius.dev>
2023-12-04 22:08:03 +0100

    new file:   fibonacci-number.py

---
+++ fibonacci-number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def fib(self, n: int, memo={}) -> int:
+        # optimized approach, using memoization
+        if n in memo:
+            return memo[n]
+        if n <= 1:
+            return n
+
+        memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
+        print(memo)
+        return memo[n]
+
+        # iterative solution
+
+    """   
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+
+        prev_1, prev_2 = 0, 1
+
+        for i in range(2, n + 1):
+            current = prev_1 + prev_2
+            prev_1, prev_2 = prev_2, current
+
+        return prev_2
+    """
