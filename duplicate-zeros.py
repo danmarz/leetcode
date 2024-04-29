d060c3a749dc8969900ea2f99082740d61a8d45f
dan m. <dm@danmarius.dev>
2024-04-30 00:12:50 +0200

    modified:   duplicate-zeros.py

--- duplicate-zeros.py
+++ duplicate-zeros.py
@@ -3,3 +3,12 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
+        i = 0
+
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.insert(i, 0)  # Insert duplicate zero at index i
+                arr.pop()  # Remove an element from the end of the array to keep it the same
+                i += 2  # Skip the inserted zeros
+            else:
+                i += 1
